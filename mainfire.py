4742eb36d1ee80d8f4eec938530d36bbf4eca3e3
searrot <83610169+searrot@users.noreply.github.com>
2021-08-05 18:45:28 +0300

    Add files via upload

---
+++ mainfire.py
@@ -0,0 +1,142 @@
+from selenium.webdriver import Chrome, Firefox
+from getpass import getpass
+from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+import urllib
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image_dataset_from_directory
+import time
+import uuid
+import os
+from selenium.webdriver.remote.webelement import WebElement
+from typing import List
+import cv2
+import pytesseract 
+from selenium.webdriver.support.ui import WebDriverWait
+
+batch_size = 32
+image_size = (254, 254)
+model = load_model('crypto_checking_network.h5')
+
+driver = webdriver.Firefox(executable_path='./projects/NEYRO/geckodriver')
+'''driver.get('https://twitter.com/login')
+time.sleep(2)
+username = driver.find_element_by_xpath('//input[@name="session[username_or_email]"]')
+username.send_keys("@IKudryavtzeff")
+time.sleep(2)
+my_password = 'Persik228'
+time.sleep(2)
+password = driver.find_element_by_xpath('//input[@name="session[password]"]')
+password.send_keys(my_password)
+password.send_keys(Keys.RETURN)'''
+time.sleep(3)
+driver.get('https://twitter.com/IKudryavtzeff')
+time.sleep(5)
+card = driver.find_element_by_xpath('//div[@data-testid="tweet"]')
+time.sleep(5)
+time_post = card.find_element_by_xpath('.//time').get_attribute('datetime')
+last_time = time_post
+trigger = False
+
+
+def get_image(container):
+    try:
+        time.sleep(2)
+        WebDriverWait(driver, 15).until(lambda container: container.find_element_by_xpath('.//img'))
+        images:List[WebElement] = container.find_elements_by_xpath('.//img')
+        del images[0]
+        if len(images) < 2:
+            imname = uuid.uuid4()
+            src = images[0].get_attribute('src')
+            urllib.request.urlretrieve(src, f'D:/_PROGRAMMING/zakaz/neyro_fl/im/ims/{imname}.jpg')
+            print('IMAGE SAVE SUCCESsS')
+        else:
+            for element in images:
+                imname = uuid.uuid4()
+                src = element.get_attribute('src')
+                urllib.request.urlretrieve(src, f'D:/_PROGRAMMING/zakaz/neyro_fl/im/ims/{imname}.jpg')
+                print('IMAGE SAVE SUCCESsS')
+    except:
+        print('ERROR SAVE IMAGE')
+        
+
+def get_text(card):
+    global trigger
+    try:
+        text = card.find_element_by_xpath('.//div[2]/div[2]/div[1]').text
+        if 'doge' in text or 'shib' in text:
+            trigger = True
+            print('NICO NICO DOUGA')
+        print(text)
+    except:
+        pass
+
+def check_image_text():
+    global trigger
+    try:
+        images = os.listdir('D:/_PROGRAMMING/zakaz/neyro_fl/im/ims/')
+        for img in images:
+            if not trigger:
+                img = cv2.imread(f'D:/_PROGRAMMING/zakaz/neyro_fl/im/ims/{img}')
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                res = pytesseract.image_to_string(img)
+                res = res.lower()
+                print(res)
+                if 'doge' in res or 'shib' in res:
+                    trigger = True
+                    print('NICO NICO DOUGA')
+    except:
+        print('text_img_ERROR')
+
+def check_image():
+    global trigger
+    try:
+        test_dataset = image_dataset_from_directory('D:/_PROGRAMMING/zakaz/neyro_fl/im/',
+                                                batch_size=batch_size,
+                                                image_size=image_size)
+        res = model.predict(test_dataset)
+        for pic in res:
+            if not trigger:
+                if pic[3] > 0.5 or pic[5] > 0.5:
+                    print('NICO NICO DOUGA')
+                    trigger = True
+    except:
+        pass
+#//div[2]/div[2]
+
+def check_tweets(l_t):
+    last_time = l_t
+    global trigger
+    while True:
+        container:WebElement = driver.find_element_by_xpath('//div[@data-testid="tweet"]')
+
+        #time.sleep(3)
+        #card = cards[0]
+        time_post = container.find_element_by_xpath('.//time').get_attribute('datetime')
+
+        if time_post != last_time:
+            get_text(container)
+            if not trigger:
+                get_image(container)
+
+                time.sleep(1)
+                check_image()
+                check_image_text()
+            trigger = False
+            for elem in os.listdir('D:/_PROGRAMMING/zakaz/neyro_fl/im/ims/'):
+                os.remove(f'D:/_PROGRAMMING/zakaz/neyro_fl/im/ims/{elem}')
+        last_time = time_post
+        driver.refresh()
+        time.sleep(3)
+
+check_tweets(last_time)
+'''while True:
+    card = driver.find_element_by_xpath('//div[@data-testid="tweet"]')
+    time_post = card.find_element_by_xpath('.//time').get_attribute('datetime')
+
+    if time_post != last_time:
+        print(get_text())
+        get_image()
+
+    last_time = time_post'''
