ae7f1c933e46dec6e9228900785efb50147adacf
searrot <eatingparrot@yandex.ru>
2021-08-09 14:55:08 +0300

    v9

--- mainfire.py
+++ mainfire.py
@@ -15,7 +15,7 @@ from typing import List
 import cv2
 import pytesseract 
 from selenium.webdriver.support.ui import WebDriverWait
-logging.basicConfig(level=logging.DEBUG)
+#logging.basicConfig(level=logging.DEBUG)
 
 batch_size = 32
 image_size = (254, 254)
@@ -24,21 +24,11 @@ model = load_model('crypto_checking_network.h5')
 options = Options()
 options.headless = True
 driver = webdriver.Firefox(executable_path='/usr/local/bin/geckodriver', options=options)
-'''driver.get('https://twitter.com/login')
-time.sleep(2)
-username = driver.find_element_by_xpath('//input[@name="session[username_or_email]"]')
-username.send_keys("@IKudryavtzeff")
-time.sleep(2)
-my_password = 'Persik228'
-time.sleep(2)
-password = driver.find_element_by_xpath('//input[@name="session[password]"]')
-password.send_keys(my_password)
-password.send_keys(Keys.RETURN)'''
-time.sleep(3)
+driver.implicitly_wait(5)
 driver.get('https://twitter.com/IKudryavtzeff')
-time.sleep(5)
+driver.implicitly_wait(5)
 card = driver.find_element_by_xpath('//div[@data-testid="tweet"]')
-time.sleep(5)
+driver.implicitly_wait(5)
 time_post = card.find_element_by_xpath('.//time').get_attribute('datetime')
 last_time = time_post
 trigger = False
@@ -46,8 +36,7 @@ trigger = False
 
 def get_image(container):
     try:
-        time.sleep(2)
-        WebDriverWait(driver, 15).until(lambda container: container.find_element_by_xpath('.//img'))
+        driver.implicitly_wait(10)
         images:List[WebElement] = container.find_elements_by_xpath('.//img')
         del images[0]
         if len(images) < 2:
@@ -72,7 +61,7 @@ def get_text(card):
         if 'doge' in text or 'shib' in text:
             trigger = True
             print('DOUG')
-            r = requests.get('http://45.137.64.175:2000/ZldaOUMyTlBiU1hFdWpYRkZUbUFFNjdv/SHIB')
+            #r = requests.get('http://45.137.64.175:2000/ZldaOUMyTlBiU1hFdWpYRkZUbUFFNjdv/SHIB')
         print(text)
     except:
         pass
@@ -91,13 +80,19 @@ def check_image_text():
                 if 'doge' in res or 'shib' in res:
                     trigger = True
                     print('DOUG')
-                    r = requests.get('http://45.137.64.175:2000/ZldaOUMyTlBiU1hFdWpYRkZUbUFFNjdv/SHIB')
+                    #r = requests.get('http://45.137.64.175:2000/ZldaOUMyTlBiU1hFdWpYRkZUbUFFNjdv/SHIB')
     except:
         print('text_img_ERROR')
 
 def check_image():
     global trigger
     try:
+        images = os.listdir('/projects/im/ims/')
+        for img in images:
+            img = cv2.imread(f'/projects/im/ims/{img}')
+            img = cv2.resize(img, (224,224), interpolation=cv2.INTER_AREA)
+            cv2.imwrite(f'/projects/im/ims/{img}.jpg', img)
+
         test_dataset = image_dataset_from_directory('/projects/im/',
                                                 batch_size=batch_size,
                                                 image_size=image_size)
@@ -106,7 +101,7 @@ def check_image():
             if not trigger:
                 if pic[3] > 0.5 or pic[5] > 0.5:
                     print('DOUG')
-                    r = requests.get('http://45.137.64.175:2000/ZldaOUMyTlBiU1hFdWpYRkZUbUFFNjdv/SHIB')
+                    #r = requests.get('http://45.137.64.175:2000/ZldaOUMyTlBiU1hFdWpYRkZUbUFFNjdv/SHIB')
                     trigger = True
     except:
         pass
@@ -118,19 +113,14 @@ def check_tweets(l_t):
     while True:
         driver.implicitly_wait(10)
         container:WebElement = driver.find_element_by_xpath('//div[@data-testid="tweet"]')
-
-        #time.sleep(3)
-        #card = cards[0]
         time_post = container.find_element_by_xpath('.//time').get_attribute('datetime')
 
         if time_post != last_time:
             get_text(container)
             if not trigger:
                 get_image(container)
-
-                time.sleep(1)
-                check_image()
                 check_image_text()
+                check_image() 
             trigger = False
             for elem in os.listdir('/projects/im/ims/'):
                 os.remove(f'/projects/im/ims/{elem}')
@@ -139,12 +129,3 @@ def check_tweets(l_t):
         driver.implicitly_wait(5)
 
 check_tweets(last_time)
-'''while True:
-    card = driver.find_element_by_xpath('//div[@data-testid="tweet"]')
-    time_post = card.find_element_by_xpath('.//time').get_attribute('datetime')
-
-    if time_post != last_time:
-        print(get_text())
-        get_image()
-
-    last_time = time_post'''
