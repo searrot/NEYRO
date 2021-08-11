8fcb3d240ebc94296413b041f559552a3ebc4aec
searrot <eatingparrot@yandex.ru>
2021-08-11 14:03:00 +0300

    v11

--- mainfire.py
+++ mainfire.py
@@ -19,7 +19,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 batch_size = 32
 image_size = (254, 254)
-f = h5py.File('cryptochecking_network.h5d', 'r')
+f = h5py.File('cryptochecking_network.h5', 'r')
 model = load_model(f)
 
 options = Options()
